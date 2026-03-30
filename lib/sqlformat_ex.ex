fe4eafde204b7a79a948b12021b632011d1160d6
Håvard Lindset <havard@givn.no>
2026-03-30 23:08:49 +0200

    initial project w/rustler and nif

---
+++ lib/sqlformat_ex.ex
@@ -0,0 +1,18 @@
+defmodule SqlformatEx do
+  @moduledoc """
+  Documentation for `SqlformatEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SqlformatEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sqlformat_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule SqlformatExTest do
+  use ExUnit.Case
+  doctest SqlformatEx
+
+  test "greets the world" do
+    assert SqlformatEx.hello() == :world
+  end
+end
